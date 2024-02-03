3529f6297faf0340432ec6e17ae314862e76ba74
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-04 00:23:17 +0530

    Create post get delete put method

--- weavy-react-app/src/weavy/UserForm.tsx
+++ weavy-react-app/src/weavy/UserForm.tsx
@@ -8,11 +8,11 @@ interface UserProps {
 
 interface UserState {
     user_id: string;
+    fisrtName: string;
+    lastName: string;
     email: string;
-    givenName: string;
-    middleName: string;
+    address:string;
     phoneNumber: string;
-    
 }
 
 export class UserForm extends Component<UserProps, UserState> {
@@ -22,13 +22,14 @@ export class UserForm extends Component<UserProps, UserState> {
 
     constructor(props: any) {
         super(props);
-        this.api = axios.create({baseURL: `https://87bf6c8a4f5442b68f1c8915bd1d3f42.weavy.io/api/`});
-        this.accessToken = "wys_3N7jqRtakTENfWF8eXqjEtsHd9Qp9C4HGEOK";
+        this.api = axios.create({baseURL: `https://feedc13e9d704cf5b65794112dc3e794.weavy.io/api`});
+        this.accessToken = "wys_7RqZlPxycruoitgvY8vfw9amgctP0q1lDe00";
         this.state = {
             user_id: "",
+            fisrtName: "",
+            lastName: "",
             email: "",
-            givenName: "",
-            middleName: "",
+            address:"",
             phoneNumber: ""
         }
         this.handleMessageInputOnChange = this.handleMessageInputOnChange.bind(this);
@@ -39,8 +40,8 @@ export class UserForm extends Component<UserProps, UserState> {
             <div>
                 <div className="flex justify-center pt-2 pb-20">
                     <div
-                        className="bg-gray-100 w-[25em] h-full mt-14 rounded-lg text-center border-2">
-                        <h1 className="text-4xl font-bold text-gray-200 bg-blue-400 pt-4 pb-4 rounded-t-lg">User
+                        className="bg-transparent flex-auto w-[80em] h-full mt-14 rounded-lg text-center border-2">
+                        <h1 className="text-4xl font-bold text-gray-200 bg-black pt-4 pb-4 rounded-t-lg">User
                             Form</h1>
 
                         <form className="">
@@ -48,9 +49,9 @@ export class UserForm extends Component<UserProps, UserState> {
 
                                 <div className="text-start ml-5">
                                     <div className="mt-5">
-                                        <label className="text-xs font-bold">User_ID</label>
+                                        <label className="text-xs font-bold mr-12">User_ID</label>
                                         <input
-                                            className="appearance-none rounded w-11/12 outline-gray-800
+                                            className="appearance-none rounded w-4/12 outline-gray-800
                                             focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
                                             id="inline-full-name" type="text"
                                             name={"user_id"}
@@ -59,52 +60,62 @@ export class UserForm extends Component<UserProps, UserState> {
                                         />
                                     </div>
 
+                            
                                     <div className="mt-5">
-                                        <label className="text-xs font-bold">Email</label>
+                                        <label className="text-xs font-bold mr-7">Fisrt Name</label>
                                         <input
-                                            className="appearance-none rounded w-11/12 outline-gray-800
+                                            className="appearance-none rounded w-4/12
                                             focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
+                                            id="inline-full-name" type="text"
+                                            name={"givenName"}
+                                            value={this.state.fisrtName}
+                                            onChange={this.handleMessageInputOnChange}
+                                        />
+                                    </div>
+
+                                    <div className="mt-5">
+                                        <label className="text-xs font-bold mr-7">Last Name</label>
+                                        <input
+                                            className="appearance-none rounded w-4/12 outline-gray-800
+                                            focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
+                                            id="inline-full-name" type="email"
+                                            name={"middleName"}
+                                            value={this.state.lastName}
+                                            onChange={this.handleMessageInputOnChange}
+                                        />
+                                    </div>
+
+                                   
+                                    <div className="mt-5">
+                                        <label className="text-xs font-bold mr-14">Email   </label>
+                                        <input
+                                            className="appearance-none rounded w-4/12 outline-gray-800
+                                            focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300 mr-1"
                                             id="inline-full-name" type="email"
                                             name={"email"}
                                             value={this.state.email}
                                             onChange={this.handleMessageInputOnChange}
                                         />
                                     </div>
-
-
+                                  
+                                          
                                     <div className="mt-5">
-                                        <label className="text-xs font-bold">Given Name</label>
+                                        <label className="text-xs font-bold mr-11">Address</label>
                                         <input
-                                            className="appearance-none rounded w-11/12
-                                            focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
-                                            id="inline-password" type="text"
-                                            name={"givenName"}
-                                            value={this.state.givenName}
-                                            onChange={this.handleMessageInputOnChange}
-                                        />
-                                    </div>
-
-                                    <div className="mt-5">
-                                        <label className="text-xs font-bold">Middle Name</label>
-                                        <input
-                                            className="appearance-none rounded w-11/12 outline-gray-800
+                                            className="appearance-none rounded w-4/12 outline-gray-800
                                             focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
                                             id="inline-full-name" type="email"
-                                            name={"middleName"}
-                                            value={this.state.middleName}
+                                            name={"address"}
+                                            value={this.state.address}
                                             onChange={this.handleMessageInputOnChange}
                                         />
                                     </div>
 
-                                   
-
-                                  
-
                                   
                                     <div className="mt-5">
-                                        <label className="text-xs font-bold">Phone Number</label>
+                                        <label className="text-xs font-bold mr-1">Phone Number</label>
                                         <input
-                                            className="appearance-none rounded w-11/12 outline-gray-800
+                                            className="appearance-none rounded w-4/12 outline-gray-800
                                             focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
                                             id="inline-full-name" type="email"
                                             name={"phoneNumber"}
@@ -117,17 +128,29 @@ export class UserForm extends Component<UserProps, UserState> {
                                 </div>
 
                                 <button
-                                    className="shadow bg-blue-400 w-11/12 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
+                                    className="shadow bg-blue-400 w-2/12 mr-2 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
                                 text-[16px] mt-5"
                                     type="button" onClick={this.onSaveBtnClick}>
                                     Save
                                 </button>
 
                                 <button
-                                    className="shadow bg-blue-400 w-11/12 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
+                                    className="shadow bg-yellow-400 w-2/12 mr-2 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
                                 text-[16px] mt-5"
-                                    type="button" /*onClick={this.checkAdmin}*/>
+                                    type="button" onClick={this.onUpdateClick}>
                                     Update
+                                </button>
+                                <button
+                                    className="shadow bg-red-400 w-2/12 mr-2 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
+                                text-[16px] mt-5"
+                                    type="button" onClick={this.onDeleteClick}>
+                                    Delete
+                                </button>
+                                <button
+                                    className="shadow bg-green-400 w-2/12  text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
+                                text-[16px] mt-5"
+                                    type="button" onClick={this.onGetAllUserClick}>
+                                    GetAll
                                 </button>
                             </div>
                         </form>
@@ -153,8 +176,9 @@ export class UserForm extends Component<UserProps, UserState> {
             this.api.post('/post', {
                     user_id: this.state.user_id,
                     email: this.state.email,
-                    givenName: this.state.givenName,
-                    middleName: this.state.middleName,
+                    fisrtName: this.state.fisrtName,
+                    lastName: this.state.lastName,
+                    address:this.state.address,
                     phoneNumber: this.state.phoneNumber,
                 },
                 {
@@ -173,5 +197,80 @@ export class UserForm extends Component<UserProps, UserState> {
         }
     }
 
+    private onUpdateClick = () =>{
+
+        try {
+            this.api.put('/put', {
+                    user_id: this.state.user_id,
+                    email: this.state.email,
+                    givenName: this.state.fisrtName,
+                    middleName: this.state.lastName,
+                    phoneNumber: this.state.phoneNumber,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${this.accessToken}`,
+                    },
+                }
+            ).then((res: { data: any }) => {
+                console.log(res);
+            }).catch((error: any) => {
+                console.error('Axios Error', error);
+            });
+        } catch (error) {
+            console.error('Error updating data:', error);
+        }
+
+    }
+
+    private onDeleteClick = () =>{
+        try {
+            this.api.delete('/delete', {
+                data: {
+                    user_id: this.state.user_id,
+                    email: this.state.email,
+                    givenName: this.state.fisrtName,
+                    middleName: this.state.lastName,
+                    phoneNumber: this.state.phoneNumber,
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${this.accessToken}`,
+                },
+            }).then((res: { data: any }) => {
+                console.log(res);
+            }).catch((error: any) => {
+                console.error('Axios Error', error);
+            });
+        } catch (error) {
+            console.error('Error deleting data:', error);
+        }
+    }
+
+    private onGetAllUserClick = async () =>{
+       
+            try {
+                const response = await this.api.get('/get', {
+                    params: {
+                        user_id: this.state.user_id,
+                        email: this.state.email,
+                        givenName: this.state.fisrtName,
+                        middleName: this.state.lastName,
+                        phoneNumber: this.state.phoneNumber,
+                    },
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${this.accessToken}`,
+                    },
+                });
+        
+                console.log(response.data);
+            } catch (error) {
+                console.error('Axios Error', error);
+            
+        }
+        
+    }
 
 }
